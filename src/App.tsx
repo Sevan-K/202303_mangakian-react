762f8692e42b1018489189fedc3e7d97b9a24f21
KABAKIAN <sevan.kabakian@gmail.com>
2023-03-26 00:21:30 +0100

    deploy for the first time

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World</h1>
+        <h1>Bienvenue sur Mangakian</h1>
       </header>
+      <main>
+        <h2>La bibliothèque de mangas intéractive</h2>
+      </main>
     </div>
   );
 }
